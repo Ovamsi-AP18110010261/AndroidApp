81a9c5814d39235209c9a85f41dfa65bc739908f
O vamsi <oruganti_vamsi@srmap.edu.in>
2020-07-02 16:49:10 +0530

    second commit

---
+++ Inputcontroltest/app/src/main/java/com/example/inputcontroltest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.inputcontroltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name,numb,pass,mail;
+    TextView txt;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.et);
+        numb=findViewById(R.id.num);
+        pass=findViewById(R.id.password);
+        mail=findViewById(R.id.email);
+        txt=findViewById(R.id.tv);
+    }
+    public void submit(View view){
+        String et = name.getText().toString();
+        String num =numb.getText().toString();
+        String email = mail.getText().toString();
+        String password =pass.getText().toString();
+
+        txt.setText(name+"\n"+numb+"\n"+mail+"\n"+pass);
+    }
+}
